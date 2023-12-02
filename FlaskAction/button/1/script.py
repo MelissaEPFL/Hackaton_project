a84d26c5a341d56a21e44adec55dbc3f455162fd
alexa <alexandre.bouillon@gmail.com>
2023-12-02 23:15:49 +0100

    git push through LOUPEDECK

--- FlaskAction/button/1/script.py
+++ FlaskAction/button/1/script.py
@@ -1,1 +1,18 @@
-print("Hello World")
+import os
+from git import Repo
+
+
+if __name__ == '__main__':
+
+    repo_path = '../../../.'
+    
+    repo = Repo(repo_path)
+    # for f in os.listdir(repo_path):
+    #     print(f)
+    origin = repo.remote(name='origin')
+    
+    origin.pull()
+    # print(repo.git.status())
+    repo.git.add('*')
+    repo.index.commit('git push through LOUPEDECK')
+    origin.push()
