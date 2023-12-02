bbfcd8f8af122205042b1f7a5ea04cce48e36539
MelissaEPFL <melissa.faggella@gmail.com>
2023-12-02 21:40:40 +0100

    last_time_on doesn't work

--- get_context.py
+++ get_context.py
@@ -11,28 +11,48 @@ def get_time_spent(name_app)->str:
     client = ActivityWatchClient(client_name="test-client", host="localhost", port=5600)
 
     # Get the events for Visual Studio Code
-    watcher = get_watcher(name_app)
-    bucket_id = f"{watcher}_{socket.gethostname()}"
+    bucket_id = f"aw-watcher-window_{socket.gethostname()}"
     events = client.get_events(bucket_id = bucket_id)
 
     # Calculate the total time spent on app
-    time_spent = 0
+    time_spent = 0.0
 
     for event in events:
-        time_spent += event["duration"] / datetime.timedelta(seconds=1)
+        if event['data']['app'] == get_process_name(name_app):
+            time_spent += event["duration"].total_seconds()
 
-           
-    return str(time_spent)
+    return str(datetime.timedelta(seconds=time_spent))
+
+
+def get_last_time_on(name_app:str)->str:
+
+    # Connect to the local ActivityWatch server
+    client = ActivityWatchClient(client_name="test-client", host="localhost", port=5600)
+
+    # Get the events for Visual Studio Code
+    bucket_id = f"aw-watcher-window_{socket.gethostname()}"
+    events = client.get_events(bucket_id = bucket_id)
+
+    #last time on name_app (events are in order)
+    for event in events:
+        if event['data']['app'] == get_process_name(name_app):
+            print(datetime.datetime.now(datetime.timezone.utc))
+            last_time = (datetime.datetime.now(datetime.timezone.utc) - event['timestamp']).total_seconds()
+
+    return str(datetime.timedelta(seconds=last_time))
+
 
 
 def get_computer_name()->str:
     return socket.gethostname()
 
 
-def get_watcher(name_app:str)->str:
+def get_process_name(name_app)->str:
 
-    if name_app == 'vscode_studio':
-        return  "aw-watcher-vscode"
-    elif name_app == 'last_pause':
-        return "aw_watcher_afk"
+    if name_app == 'vscode':
+        return  "devenv.exe"
+    elif name_app == 'vscode_studio':
+        return "Code.exe"
+ 
+
  
