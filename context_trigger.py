bf0358bfc9d59d6910c8eb7afaf029e2adab1229
Rom1 <31048092+RomainMendez@users.noreply.github.com>
2023-12-03 09:32:40 +0100

    git push through LOUPEDECK

--- context_trigger.py
+++ context_trigger.py
@@ -1,11 +1,59 @@
 import datetime
-import mailbox
-
 from get_context import *
+import imaplib
+import ssl
+import pickle
 
+def get_number_unread_emails()->int:
+    # Connect to Gmail IMAP server
 
+    custom_port = 2143  # Change this to your custom port number
+    mail = imaplib.IMAP4('127.0.0.1', custom_port)
+    # Start TLS to upgrade the connection to a secure one
+    mail.starttls()
+
+    # Login to your account
+    username = 'romain.mendez@protonmail.com'
+    password = 'Wx9szu7PmzLgW4pc_0wgxA'
+    mail.login(username, password)
+
+    # Select the mailbox you want to check (e.g., 'INBOX')
+    mail.select('INBOX')
+
+    # Search for unseen emails
+    status, response = mail.search(None, 'UNSEEN')
+    print(response)
+    # Get the list of unseen emails
+    unseen_emails = len(response[0].decode(encoding="utf-8").split(" "))
+
+    if unseen_emails != 0:
+        print(f"You have {unseen_emails} unseen emails!")
+        # Process or fetch these emails if needed
+
+    # Logout from the server
+    mail.logout()
+    return unseen_emails
 
 def trigger_thunderbird(trigger_time = datetime.timedelta(seconds=15))->bool:
+    def load_integer_from_pickle(file_path: str) -> int:
+        with open(file_path, 'rb') as file:
+            integer = pickle.load(file)
+        return integer
+    
+    unread_emails = get_number_unread_emails()
+    
+    try:
+        last_number_of_emails_unseen = load_integer_from_pickle("./last_time_on_thunderbird.pickle")
+    except:
+        with open("./last_time_on_thunderbird.pickle", 'wb') as file:
+            pickle.dump(unread_emails, file)
+        last_number_of_emails_unseen = load_integer_from_pickle("./last_time_on_thunderbird.pickle")
+        
+    if unread_emails > last_number_of_emails_unseen:
+        return True
+    else:
+        return False
+    
 
     #Check when was the last time you were on Thunderbird
     last_time_on = get_last_time_on("thunderbird_mail")
@@ -14,7 +62,7 @@ def trigger_thunderbird(trigger_time = datetime.timedelta(seconds=15))->bool:
 
 
     if last_time_on > trigger_time:
-        if 
+        if True:
             return True
     else: 
         return False
