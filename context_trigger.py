af9eb3c330fe06623941d2e9fb99cccdc346f814
MelissaEPFL <melissa.faggella@gmail.com>
2023-12-03 09:08:31 +0100

    thunderbird new condition on unread mail

--- context_trigger.py
+++ context_trigger.py
@@ -1,14 +1,21 @@
 import datetime
+import mailbox
+
 from get_context import *
+
+
 
 def trigger_thunderbird(trigger_time = datetime.timedelta(seconds=15))->bool:
 
+    #Check when was the last time you were on Thunderbird
     last_time_on = get_last_time_on("thunderbird_mail")
-    print(type(last_time_on))
+    
+    #Check if at least one unread 
+
 
     if last_time_on > trigger_time:
-        print("Time to check email bitch !")
-        return True
+        if 
+            return True
     else: 
         return False
 
