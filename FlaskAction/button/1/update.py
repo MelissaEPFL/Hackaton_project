c4177736c293cd3074ab1ccf5fa9d520e2ad32b6
Rom1 <31048092+RomainMendez@users.noreply.github.com>
2023-12-03 10:20:03 +0100

    git push through LOUPEDECK

--- FlaskAction/button/1/update.py
+++ FlaskAction/button/1/update.py
@@ -8,7 +8,6 @@ def main_update():
         
     if repo.is_dirty(untracked_files=True):
         print("There are unstaged changes in the repository.")
-        return "rickroll-roll.gif"
         return "exploded.jpg"
     else:
         print("There are no unstaged changes in the repository.")
