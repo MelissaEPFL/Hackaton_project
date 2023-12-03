6783a21b505cf929a2f91a592766b099b35d18ec
Rom1 <31048092+RomainMendez@users.noreply.github.com>
2023-12-03 08:56:13 +0100

    git push through LOUPEDECK

---
+++ FlaskAction/button/1/update.py
@@ -0,0 +1,14 @@
+from git import Repo
+
+repo_path = '../'
+repo = Repo(repo_path)
+
+
+def main_update():
+        
+    if repo.is_dirty(untracked_files=True):
+        print("There are unstaged changes in the repository.")
+        return "exploded.jpg"
+    else:
+        print("There are no unstaged changes in the repository.")
+        return "image.jpg"
